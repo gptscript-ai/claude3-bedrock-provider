65e9fe752e7a7fc7eb80e7690cd5399e3ae3b7ab
Taylor Price <tayworm@gmail.com>
2024-05-22 13:35:20 -0700

    chore!: initial commit after split of anthropic-provider
    Signed-off-by: Taylor Price <tayworm@gmail.com>

---
+++ main.py
@@ -0,0 +1,52 @@
+import json
+import os
+
+import claudelib
+from anthropic import AsyncAnthropicBedrock
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse, StreamingResponse
+
+debug = os.environ.get("GPTSCRIPT_DEBUG", "false") == "true"
+client = AsyncAnthropicBedrock()
+app = FastAPI()
+
+
+def log(*args):
+    if debug:
+        print(*args)
+
+
+@app.middleware("http")
+async def log_body(request: Request, call_next):
+    body = await request.body()
+    log("HTTP REQUEST BODY: ", body)
+    return await call_next(request)
+
+
+@app.post("/")
+async def post_root():
+    return 'ok'
+
+
+@app.get("/")
+async def get_root():
+    return 'ok'
+
+
+@app.get("/v1/models")
+async def list_models() -> JSONResponse:
+    return await claudelib.list_models(client)
+
+
+@app.post("/v1/chat/completions")
+async def completions(request: Request) -> StreamingResponse:
+    data = await request.body()
+    input = json.loads(data)
+    return await claudelib.completions(client, input)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="127.0.0.1", port=int(os.environ.get("PORT", "8000")),
+                log_level="debug" if debug else "critical", reload=debug, access_log=debug)
